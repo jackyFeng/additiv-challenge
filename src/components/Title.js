2b891efddb2257120db5e4289826fd253d57057c
Xin Feng <xinfeng@Xins-MacBook-Pro.local>
2019-07-07 15:19:47 +0800

    remove console logs

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
-const Title = ({title}) => {
-  return (
-    <header className="header">
-      {title}
-    </header>
-  )
-}
+const Title = ({ title }) => {
+  return <header className="header">{title}</header>;
+};
 
 export default Title;
